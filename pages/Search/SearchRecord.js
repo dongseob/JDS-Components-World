213559ce02c4290eaa0a534a98139de28b2ad8e5
dongseob <jds3568@naver.com>
2023-01-12 22:20:53 +0900

    deploy error

--- pages/Search/SearchRecord.js
+++ pages/Search/SearchRecord.js
@@ -12,9 +12,10 @@ const SearchRecord = () => {
     const fnSubmit = (e) => {
         e.preventDefault();
         const origin = window.localStorage.getItem("최근검색어");
-        const result = origin.split(",");
+        const result = [];
+        if (origin !== null) result = origin.split(",");
 
-        if (origin.length > 0) {
+        if (origin !== null) {
             result.push(searchValue);
             window.localStorage.setItem("최근검색어", result);
             setSearchRecord(result);
@@ -30,7 +31,8 @@ const SearchRecord = () => {
 
     useEffect(() => {
         const origin = window.localStorage.getItem("최근검색어");
-        const result = origin.split(",");
+        const result = [];
+        if (origin !== null) result = origin.split(",");
         setSearchRecord(result);
     }, []);
 
@@ -49,7 +51,11 @@ const SearchRecord = () => {
                         className="w-full border-none"
                         placeholder="검색어를 입력해주세요"
                         onFocus={() => setIsSearch(true)}
-                        onBlur={() => setTimeout(() => {setIsSearch(false)}, 200) } //밑의 a태그를 누를 때 보다 빨리 동작해서 지연걸어놈 => 좋은 방법 아님
+                        onBlur={() =>
+                            setTimeout(() => {
+                                setIsSearch(false);
+                            }, 200)
+                        } //밑의 a태그를 누를 때 보다 빨리 동작해서 지연걸어놈 => 좋은 방법 아님
                     />
                     <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
                         <button
@@ -63,30 +69,43 @@ const SearchRecord = () => {
 
                 {isSearch && (
                     <div className="py-2 bg-white border-t-2">
-                        {searchRecord
-                            .slice(0)
-                            .reverse()
-                            .map((el) => (
-                                <>
-                                    <a
-                                        href="#"
-                                        className="w-full bg-white mt-[1px] py-2 px-3 flex text-sm items-center border-none outline-none hover:bg-slate-200 focus:bg-slate-200 transition-all"
-                                        onClick={() => alert(el)}
-                                    >
-                                        <p>{el}</p>
-                                        {/* 탭 동작에 걸리기 때문에 버튼이 아닌 div로 */}
-                                        <div
-                                            className="ml-auto border-2 px-2 border-slate-500 rounded-md hover:bg-slate-300"
-                                            onClick={(e) => {
-                                                e.stopPropagation();
-                                                alert(el);
-                                            }}
-                                        >
-                                            X
-                                        </div>
-                                    </a>
-                                </>
-                            ))}
+                        <div
+                            onClick={() => {
+                                window.localStorage.removeItem("최근검색어");
+                                alert("최근검색어가 모두 삭제되었습니다.");
+                            }}
+                            className="text-xl font-semibold text-gray-500 cursor-pointer ml-2 mb-2"
+                        >
+                            전체 삭제
+                        </div>
+                        {searchRecord && (
+                            <>
+                                {searchRecord
+                                    .slice(0)
+                                    .reverse()
+                                    .map((el) => (
+                                        <>
+                                            <a
+                                                href="#"
+                                                className="w-full bg-white mt-[1px] py-2 px-3 flex text-sm items-center border-none outline-none hover:bg-slate-200 focus:bg-slate-200 transition-all"
+                                                onClick={() => alert(el)}
+                                            >
+                                                <p>{el}</p>
+                                                {/* 탭 동작에 걸리기 때문에 버튼이 아닌 div로 */}
+                                                <div
+                                                    className="ml-auto border-2 px-2 border-slate-500 rounded-md hover:bg-slate-300"
+                                                    onClick={(e) => {
+                                                        e.stopPropagation();
+                                                        alert(el);
+                                                    }}
+                                                >
+                                                    X
+                                                </div>
+                                            </a>
+                                        </>
+                                    ))}
+                            </>
+                        )}
                     </div>
                 )}
             </div>
