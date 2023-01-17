46c4353dbcf2f4d377f16e4da93f988c503dff6c
dongseob <jds3568@naver.com>
2023-01-18 00:13:36 +0900

    search > searchRecord(최근검색어) 기능 구현

--- pages/Search/Search.js
+++ pages/Search/Search.js
@@ -10,7 +10,7 @@ const Search = () => {
                 <span className="back">&larr;</span>
             </Link>
             <div className="text-2xl font-semibold mt-2">Search</div>
-            <div className="card text-sm">💡&emsp;기능 참고 : 구글 검색창</div>
+            <div className="card text-sm">💡&emsp;기능 참고 : 구글 검색창, 네이버 검색창</div>
             <SearchRecord></SearchRecord>
             <SearchAuto></SearchAuto>
         </div>
--- pages/Search/SearchRecord.js
+++ pages/Search/SearchRecord.js
@@ -1,39 +1,77 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const SearchRecord = () => {
-    const [searchValue, setSearchValue] = useState("");
-    const [searchRecord, setSearchRecord] = useState([]);
-    const [isSearch, setIsSearch] = useState(false);
+    const [isInput, setIsInput] = useState(true); // 최근검색어 화면을 띄울지 말지 결정
+    const [isInputVal, setIsInputVal] = useState(""); // 인풋에 담긴 값 실시간 저장
+    const [storageItem, setStorageItem] = useState(null);
+    const inputRef = useRef(null);
 
-    const fnInputChange = useCallback((e) => {
-        setSearchValue(e.target.value);
-    }, []);
+    // 검색 실행 함수
+    const fnSubmit = (e, val) => {
+        e.preventDefault(); // 페이지 이동 막음
 
-    const fnSubmit = (e) => {
-        e.preventDefault();
-        const origin = window.localStorage.getItem("최근검색어");
-        const result = [];
-        if (origin !== null) result = origin.split(",");
+        if (val === "" || val.length === 0) {
+            alert("검색어를 입력해주세요");
+            return;
+        }
 
-        if (origin !== null) {
-            result.push(searchValue);
-            window.localStorage.setItem("최근검색어", result);
-            setSearchRecord(result);
+        addStorage(e, val); // 아이템 저장 함수
+
+        // alert(val + "로 검색하셨군요 검색결과 화면은 준비중입니다.");
+    };
+
+    // 로컬스토리지에 아이템 저장
+    const addStorage = (e, val) => {
+        let storageItem = JSON.parse(localStorage.getItem("최근검색어"));
+
+        console.log("yogiyo1 : ", storageItem);
+
+        if (storageItem !== null) {
+            // 로컬스토리지에 값이 존재한지 확인
+            if (storageItem.includes(val)) {
+                // 입력한 값이 기존에 존재하는 아이템이라면 삭제한 후, 다시 저장
+                removeStorage(e, val); // 중복 아이템 제거
+
+                let storageItem = JSON.parse(
+                    localStorage.getItem("최근검색어")
+                );
+                storageItem.push(val); // 기존값과 합침
+                localStorage.setItem("최근검색어", JSON.stringify(storageItem)); // 로컬스토리지에 기존값과 합쳐서 저장
+                setStorageItem(storageItem);
+            } else {
+                // 입력한 값이 기존에 존재하지 않다면
+                storageItem.push(val); // 기존값과 합침
+                localStorage.setItem("최근검색어", JSON.stringify(storageItem)); // 로컬스토리지에 기존값과 합쳐서 저장
+                setStorageItem(storageItem);
+            }
         } else {
-            window.localStorage.setItem("최근검색어", searchValue);
-            setSearchRecord(searchValue);
+            localStorage.setItem("최근검색어", val); // 로컬스토리지에 아무값도 없을때 저장
+            setStorageItem(storageItem);
         }
     };
 
-    // useEffect(() => {
-    //     console.log(searchRecord);
-    // }, [searchRecord]);
+    // 로컬스토리지에 아이템 제거
+    const removeStorage = (e, val) => {
+        // 선택한 값을 제외한 배열 다시 생성하여 저장하는 프로세스
+        let storageItem = JSON.parse(localStorage.getItem("최근검색어"));
+        let result = storageItem.filter((el) => el !== val);
+        localStorage.setItem("최근검색어", JSON.stringify(result));
+        setStorageItem(result);
+        e.stopPropagation(); // 이벤트 버블링 방지
+    };
 
     useEffect(() => {
-        const origin = window.localStorage.getItem("최근검색어");
-        const result = [];
-        if (origin !== null) result = origin.split(",");
-        setSearchRecord(result);
+        // 값이 없으면 초기화
+        if (localStorage.getItem("최근검색어") === null) {
+            localStorage.setItem("최근검색어", JSON.stringify([])); //초기화 용도
+        }
+
+        let storageItem = JSON.parse(localStorage.getItem("최근검색어"));
+
+        // 최초 스토리지에 아이템이 있다면 셋팅
+        if (storageItem !== null) {
+            setStorageItem(storageItem);
+        }
     }, []);
 
     return (
@@ -42,72 +80,71 @@ const SearchRecord = () => {
 
             <div className="mt-4">
                 <form
-                    onSubmit={fnSubmit}
-                    className="relative mt-1 flex items-center"
+                    onSubmit={(e) => fnSubmit(e, isInputVal)}
+                    className="px-[18px] relative flex"
                 >
                     <input
                         type="text"
-                        onChange={fnInputChange}
-                        className="w-full border-none"
-                        placeholder="검색어를 입력해주세요"
-                        onFocus={() => setIsSearch(true)}
-                        onBlur={() =>
-                            setTimeout(() => {
-                                setIsSearch(false);
-                            }, 200)
-                        } //밑의 a태그를 누를 때 보다 빨리 동작해서 지연걸어놈 => 좋은 방법 아님
+                        placeholder="검색어를 입력하세요"
+                        onClick={() => setIsInput(false)}
+                        onChange={(e) => setIsInputVal(e.target.value)}
+                        ref={inputRef}
+                        className="h-[44px] bg-[#eeeeee] rounded-[12px] py-[8px] px-[16px] pr-[140px] placeholder:text-[16px] placeholder:text-[#888888] placeholder:text-font-semibold w-full"
                     />
-                    <div className="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
-                        <button
-                            type="submit"
-                            className="inline-flex items-center rounded border border-gray-200 px-2 font-sans text-sm font-medium text-gray-400"
-                        >
-                            Search
-                        </button>
-                    </div>
-                </form>
 
-                {isSearch && (
-                    <div className="py-2 bg-white border-t-2">
-                        <div
+                    <button
+                        type="submit"
+                        className="flex absolute right-[28px] self-center rounded-md border border-transparent bg-indigo-600 px-2 py-1 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    >
+                        검색
+                    </button>
+                    {isInputVal.length > 0 && (
+                        // 검색 인풋에 값을 1글자 이상 넣을 때 출력, isInputVal 초기화 용도
+                        <button
                             onClick={() => {
-                                window.localStorage.removeItem("최근검색어");
-                                alert("최근검색어가 모두 삭제되었습니다.");
+                                setIsInputVal("");
+                                inputRef.current.value = "";
                             }}
-                            className="text-xl font-semibold text-gray-500 cursor-pointer ml-2 mb-2"
+                            type="submit"
+                            className="flex absolute right-[80px] self-center rounded-md border border-transparent bg-indigo-600 px-2 py-1 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                         >
-                            전체 삭제
-                        </div>
-                        {searchRecord && (
+                            지우기
+                        </button>
+                    )}
+                </form>
+                <div className="px-[18px] my-[24px]">
+                    <p className="font-bold">최근 검색 목록</p>
+                    <ul className="mt-[12px]">
+                        {storageItem !== null && (
                             <>
-                                {searchRecord
-                                    .slice(0)
-                                    .reverse()
+                                {storageItem
+                                    .slice(0) // 배열 반전
+                                    .reverse() // 배열 반전
                                     .map((el) => (
-                                        <>
-                                            <a
-                                                href="#"
-                                                className="w-full bg-white mt-[1px] py-2 px-3 flex text-sm items-center border-none outline-none hover:bg-slate-200 focus:bg-slate-200 transition-all"
-                                                onClick={() => alert(el)}
+                                        <li
+                                            className="py-[12px] border-b flex relative"
+                                            key={el}
+                                        >
+                                            <p
+                                                onClick={(e) => fnSubmit(e, el)} // 검색 및 아이템 저장 함수
                                             >
-                                                <p>{el}</p>
-                                                {/* 탭 동작에 걸리기 때문에 버튼이 아닌 div로 */}
-                                                <div
-                                                    className="ml-auto border-2 px-2 border-slate-500 rounded-md hover:bg-slate-300"
-                                                    onClick={(e) => {
-                                                        e.stopPropagation();
-                                                        alert(el);
-                                                    }}
-                                                >
-                                                    X
-                                                </div>
-                                            </a>
-                                        </>
+                                                {el}
+                                            </p>
+
+                                            <button
+                                                onClick={(e) =>
+                                                    removeStorage(e, el)
+                                                } // 아이템 삭제 함수
+                                                className="flex absolute right-0 self-center rounded-md border border-transparent bg-indigo-600 px-2 py-1 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                            >
+                                                제거
+                                            </button>
+                                        </li>
                                     ))}
                             </>
                         )}
-                    </div>
-                )}
+                    </ul>
+                </div>
             </div>
         </div>
     );
